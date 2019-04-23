cd630ed2ffadb723c8311bfbb791a1235675a4ab
jiawenzzz <45322000+jiawenzzz@users.noreply.github.com>
2019-04-23 14:17:41 -0400

    Add files via upload

--- Animation.m
+++ Animation.m
@@ -1,18 +1,14 @@
+%%This function is to visualize the parking process
 function Animation(Y_ref)
 figure
-% <<<<<<< HEAD
-% Obstacle1_x = [1,3,3,1,1,3,1,3]*1.3;
-% Obstacle1_y = [-1,-1,1,1,-1,1,1,-1] * 2.25;
-% Obstacle2_x = [1,3,3,1,1,3,1,3] *1.3* -1;
-% vehicle_shape = [4.2, 1.6]; % length x width
-% =======
 Obstacle1_x = [1.3,3.9,3.9,1.3,1.3,3.9,1.3,3.9];
 Obstacle1_y = [-1,-1,1,1,-1,1,1,-1] * 2.7;
 Obstacle2_x = [1.3,3.9,3.9,1.3,1.3,3.9,1.3,3.9] * -1;
 vehicle_shape = [4.45, 1.8]; % length x width
-% >>>>>>> d5addcbe4906f3c7bf10e257da56e1b9b712d8d3
+
 for t = 1:10:length(Y_ref) - 1
-    plot(Obstacle1_x, Obstacle1_y,'k');hold on;
+    %plot two adjacent parking spaces
+    plot(Obstacle1_x, Obstacle1_y,'k');hold on; 
     plot(Obstacle2_x, Obstacle1_y,'k');hold on;
     x = Y_ref(1,t);
     y = Y_ref(2,t);
